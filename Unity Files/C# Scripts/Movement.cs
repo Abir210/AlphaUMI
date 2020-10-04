391a24786f5f1b3ce960f1e50d9068bfd10f4e29
Abir <50152575+Abir210@users.noreply.github.com>
2020-10-04 05:54:15 +0100

    Add files via upload
    C# Scripts used in developing dialogues

---
+++ Unity Files/C# Scripts/Movement.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+	public Rigidbody lm;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        lm.AddForce(0, 0, 200 * Time.deltaTime);
+    }
+}
